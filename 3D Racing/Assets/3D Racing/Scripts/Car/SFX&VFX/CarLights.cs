347f8048997f3453c318d5c9e9d744269ca68199
Maxim Shirokov <shirokmax@mail.ru>
2023-11-29 13:30:04 +0700

    Доработка визуала для префаба машины (свет фар и неон) + скрипт включения/выключения фар/неона)

---
+++ 3D Racing/Assets/3D Racing/Scripts/Car/SFX&VFX/CarLights.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace CarRacing
+{
+    public enum CarLightType
+    {
+        Default,
+        Neon
+    }
+
+    public class CarLights : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_DefaultLight;
+        [SerializeField] private GameObject m_NeonLight;
+
+        [SerializeField] private CarLightType m_Type;
+        public CarLightType Type => m_Type;
+        public void SetLightType(CarLightType type) => m_Type = type;
+
+        private void Update()
+        {
+            SwitchLight();
+        }
+
+        private void SwitchLight()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                if (m_Type == CarLightType.Default)
+                {
+                    m_DefaultLight.SetActive(!m_DefaultLight.activeSelf);
+                    m_NeonLight.SetActive(false);
+                }
+
+                if (m_Type == CarLightType.Neon)
+                {
+                    m_NeonLight.SetActive(!m_NeonLight.activeSelf);
+                    m_DefaultLight.SetActive(false);
+                }
+            }
+        }
+    }
+}
