b702b72d67caf144620f37e3f3e01058e5d09060
Maxim Shirokov <shirokmax@mail.ru>
2023-12-08 10:35:23 +0700

    Корректное закрытие панелей главного меню

--- 3D Racing/Assets/3D Racing/Scripts/UI/UIMainMenuGUI.cs
+++ 3D Racing/Assets/3D Racing/Scripts/UI/UIMainMenuGUI.cs
@@ -9,17 +9,22 @@ namespace CarRacing
         [SerializeField] private UISelectableButtonContainer m_MenuItemsBarContainer;
         [SerializeField] private UISounds m_Sounds;
 
+        [SerializeField] private GameObject[] m_LevelPanels;
+
         private void Start()
         {
             m_MainCanvas.SetActive(false);
             m_MainMenuCanvas.SetActive(true);
+
+            foreach (var panel in m_LevelPanels)
+                panel.SetActive(false);
         }
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                if (m_MainCanvas.activeSelf == true)
+                if (m_MainCanvas.activeSelf == true && IsLevelPanelsClosed() == true)
                 {
                     ExitToMainMenu();
                     m_Sounds.PlaySound(SoundType.Back);
@@ -32,6 +37,22 @@ namespace CarRacing
             m_MenuItemsBarContainer.SelectFirstButton();
             m_MainCanvas.SetActive(false);
             m_MainMenuCanvas.SetActive(true);
+        }
+
+        private bool IsLevelPanelsClosed()
+        {
+            foreach (var panel in m_LevelPanels)
+            {
+                if (panel.activeSelf == true)
+                {
+                    panel.SetActive(false);
+                    m_Sounds.PlaySound(SoundType.Back);
+
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
